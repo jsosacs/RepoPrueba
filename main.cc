b50b617fed2afa7687c0c058d51d894519514b73
Javier <alu0100885989@ull.edu.es>
2023-01-19 14:32:21 +0000

    nomain y pepito con cosas, main segundo commit

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 #include <iostream>
 
 int main() {
-    std::cout << "hola" << std::endl;
+    std::cout << "hola segundo commit" << std::endl;
 }
--- main3.cc
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "hola 3" << std::endl;
-}
